e02c1ec4bdfc057f12648395169b373f68094f02
Esmond Lam <esmondlam0923@gmail.com>
2024-03-18 15:12:36 +0800

    Hook to an ui and display result through DOM manipulation instead of console

--- index.js
+++ index.js
@@ -1,27 +1,74 @@
-function playAGame() {
-  let playerScore = 0;
-  let computerScore = 0;
-  for (let i = 0; i < 5; i++) {
-    // const playerChoice = prompt("What's your choice this round?");
-    const computerChoice = getComputerChoice();
-    let result = playARound(playerChoice, computerChoice);
-    console.log(result);
-    if (result.includes("Win")) playerScore++;
-    if (result.includes("Lose")) computerScore++;
+const contentContainer = document.querySelector(".content-container");
+const btns = document.querySelectorAll(".btns-container button");
+const playerScoreText = document.querySelector("#player");
+const computerScoreText = document.querySelector("#computer");
+
+let playerScore = 0;
+let computerScore = 0;
+
+for (const btn of btns) {
+  btn.addEventListener("click", (event) => {
+    playAGame(event.target.textContent);
+  });
+}
+
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", reset);
+
+function reset() {
+  playerScore = 0;
+  computerScore = 0;
+
+  while (contentContainer.firstChild) {
+    contentContainer.removeChild(contentContainer.firstChild);
   }
 
-  if (playerScore === computerScore) {
-    console.log("You have tied with the computer!");
-  } else {
-    let winner =
-      playerScore > computerScore ? "You are the Winner!" : "You lose...";
-    console.log(winner);
+  playerScoreText.textContent = "0";
+  computerScoreText.textContent = "0";
+
+  for (const btn of btns) {
+    btn.disabled = false;
+  }
+}
+
+function playAGame(playerChoice) {
+  const computerChoice = getComputerChoice();
+  let result = playARound(playerChoice, computerChoice);
+  console.log(result);
+  if (result.includes("win")) playerScore++;
+  if (result.includes("lose")) computerScore++;
+
+  const resultTxt = document.createElement("p");
+  resultTxt.textContent = result;
+  contentContainer.appendChild(resultTxt);
+  contentContainer.scrollTop = contentContainer.scrollHeight;
+
+  playerScoreText.textContent = playerScore;
+  computerScoreText.textContent = computerScore;
+
+  endGame();
+}
+
+function endGame() {
+  if (playerScore === 5) {
+    contentContainer.textContent = "You have won!";
+    for (const btn of btns) {
+      btn.disabled = true;
+    }
+  }
+
+  if (computerScore === 5) {
+    contentContainer.textContent = "You have lost...";
+    for (const btn of btns) {
+      btn.disabled = true;
+    }
   }
 }
 
 function getComputerChoice() {
   const CHOICE = ["rock", "paper", "scissors"];
   const randomIndex = Math.floor(Math.random() * CHOICE.length);
+  console.log(randomIndex);
   return CHOICE[randomIndex];
 }
 
@@ -29,14 +76,14 @@ function playARound(playerSelection, computerSelection) {
   const validPlayerSelection = playerSelection.toLowerCase();
   let result;
 
-  if (playerSelection === computerSelection) result = "Tie!";
+  if (validPlayerSelection === computerSelection) result = "Tie!";
 
   if (
     (validPlayerSelection === "rock" && computerSelection === "scissors") ||
     (validPlayerSelection === "scissors" && computerSelection === "paper") ||
-    (validPlayerSelection === "paper" && computerSelection === "rocks")
+    (validPlayerSelection === "paper" && computerSelection === "rock")
   ) {
-    result = `You Win! ${validPlayerSelection} beats ${computerSelection}`;
+    result = `You win! ${validPlayerSelection} beats ${computerSelection}`;
   }
 
   if (
@@ -49,5 +96,3 @@ function playARound(playerSelection, computerSelection) {
 
   return result;
 }
-
-playAGame();
